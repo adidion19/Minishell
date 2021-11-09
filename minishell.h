1415729d1ac6e464af014f6705d4841111acee64
adidion19 <didionantoine@gmail.com>
2021-11-09 14:58:33 +0100

    atoi et itoa a la norme et ranges

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: adidion <adidion@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 11:15:21 by adidion           #+#    #+#             */
-/*   Updated: 2021/11/09 14:24:18 by adidion          ###   ########.fr       */
+/*   Updated: 2021/11/09 14:58:07 by adidion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,37 +20,55 @@
 # include <readline/history.h>
 # include <unistd.h>
 
-int		ft_pwd(int bool);
-int		ft_env(char **env);
-int		ft_atoi(const char *str);
-char	*ft_itoa(int n);
-int		ft_cd(char *path, char **pwd, char **oldpwd);
+int				ft_pwd(int bool);
+int				ft_env(char **env);
+int				ft_atoi(const char *str);
+char			*ft_itoa(int n);
+int				ft_cd(char *path, char **pwd, char **oldpwd);
+
+/*
+**	FT_ATOI
+*/
+
+int				ft_isspace(char c);
+long			ft_return_of_strtol(int neg);
+long			ft_strtol(const char *str);
+int				ft_atoi(const char *str);
+
+/*
+**	FT_ITOA
+*/
+
+char			*ft_strrev(char *str);
+int				len_of_int(int n);
+unsigned int	a_value_of_itoa(unsigned int a, int n);
+char			*ft_itoa(int n);
 
 /*
 **	UTILS (COMPLET)
 */
-size_t	len_lst(char **lst);
-size_t	ft_strlen(char *str);
-char	*ft_strncpy(char *src, char *dst, int nb);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strcpy_after(char *src, char *after);
+size_t			len_lst(char **lst);
+size_t			ft_strlen(char *str);
+char			*ft_strncpy(char *src, char *dst, int nb);
+int				ft_strncmp(const char *s1, const char *s2, size_t n);
+char			*ft_strcpy_after(char *src, char *after);
 
 /*
 **	UTILS_B
 */
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strdup(const char *src);
+char			*ft_strjoin(char *s1, char *s2);
+char			*ft_strdup(const char *src);
 
 /*
 **	ERROR
 */
-char	**free_env(char **env, int nb);
+char			**free_env(char **env, int nb);
 
 /*
 **	INIT_ENV
 */
-int		env_replace(char **new_env, char *type, char *replace);
-char	*env_find_the(char *search, char **src);
-char	**init_env(char **env);
+int				env_replace(char **new_env, char *type, char *replace);
+char			*env_find_the(char *search, char **src);
+char			**init_env(char **env);
 
 #endif
--- utils/ft_itoa.c
+++ utils/ft_itoa.c
@@ -6,13 +6,13 @@
 /*   By: adidion <adidion@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 16:18:56 by adidion           #+#    #+#             */
-/*   Updated: 2021/10/29 16:19:26 by adidion          ###   ########.fr       */
+/*   Updated: 2021/11/09 14:54:40 by adidion          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static char	*ft_strrev(char *str)
+char	*ft_strrev(char *str)
 {
 	int		i;
 	int		len;
@@ -34,9 +34,9 @@ static char	*ft_strrev(char *str)
 	return (str);
 }
 
-static int	len_of_int(int n)
+int	len_of_int(int n)
 {
-	int count;
+	int	count;
 
 	count = 0;
 	if (n < 0)
@@ -53,6 +53,15 @@ static int	len_of_int(int n)
 	return (count);
 }
 
+unsigned int	a_value_of_itoa(unsigned int a, int n)
+{
+	if (n >= 0)
+		a = n;
+	else
+		a = -n;
+	return (a);
+}
+
 char	*ft_itoa(int n)
 {
 	char			*ans;
@@ -62,11 +71,12 @@ char	*ft_itoa(int n)
 
 	bool_ = 1;
 	i = 0;
-	if (!(ans = (char*)malloc(sizeof(char) * (len_of_int(n) + 1))))
+	ans = (char *)malloc(sizeof(char) * (len_of_int(n) + 1));
+	if (ans)
 		return (0);
 	if (n < 0)
 		bool_ = 0;
-	a = (n >= 0) ? n : -n;
+	a = a_value_of_itoa(a, n);
 	while (a >= 10)
 	{
 		ans[i++] = a % 10 + 48;
