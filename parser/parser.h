8b3ba024ce15706128b22842e2355b6987133396
artmende <artmende@student.s19.be>
2021-11-15 15:02:52 +0100

    quote struct back to parser include, add word linked list struct too

--- parser/parser.h
+++ parser/parser.h
@@ -6,12 +6,25 @@
 /*   By: artmende <artmende@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 15:53:08 by artmende          #+#    #+#             */
-/*   Updated: 2021/11/15 14:26:54 by artmende         ###   ########.fr       */
+/*   Updated: 2021/11/15 15:01:17 by artmende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
+
+typedef struct s_words_list
+{
+	char					*word;
+	struct	s_words_list	next;
+}	t_words_list;
+
+typedef struct s_quote_state
+{
+	int	simple_quote;
+	int	double_quote;
+	int	global_quote;
+}	t_quote_state;
 
 /* typedef struct s_pipe_list
 {
